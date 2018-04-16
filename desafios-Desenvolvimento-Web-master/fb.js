a57c1c3834c932867fa55a3e5ff21262d3eb33ea
GabrielMDze <36632008+GabrielMDze@users.noreply.github.com>
2018-04-16 11:09:57 -0300

    Add files via upload

---
+++ desafios-Desenvolvimento-Web-master/fb.js
@@ -0,0 +1,18 @@
+function fizzbuzz() {
+    for (var i = 1; i <= 100; i++) {
+
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("fizzbuzz");
+            continue;
+        }
+        if (i % 3 === 0) {
+            console.log("fizz");
+            continue;
+        }
+        if (i % 5 === 0) {
+            console.log("buzz");
+            continue;
+        }
+        
+    }
+}
--- desafios-Desenvolvimento-Web-master/inverter.js
+++ desafios-Desenvolvimento-Web-master/inverter.js
@@ -2,3 +2,14 @@ function solution(str) {
     return str.split('').reverse().join('');
 }
 console.log(solution('Matheus')); 
+
+
+//
+
+function reverseString(str) {
+    var reverse = "";
+    for (var i = str.length - i; i >= 0; i--) {
+        reverse += str[i];
+    }
+    return reverse;
+}
